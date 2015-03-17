3b419f3065a9171c42c869421ec3da9fb8e77615
couturier-d <couturier-d@ACR-M4610G-317.tec.saint-louis.local>
2015-03-17 14:46:23 +0100

    commit 2

--- src/test/test.java
+++ src/test/test.java
@@ -6,10 +6,11 @@ public class test {
 		
 	}
 	/**
-	 * Point d'entrée de l'application
+	 * Point d'entrée de l'application.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("Projet existant.");
+		int nombre = 0;
 	}
 }
